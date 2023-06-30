879bebf560d5956d297682f5ee1e8e698ab7fc49
jonahbrawley <jonahf.brawley@calbaptist.edu>
2023-06-30 16:58:39 -0700

    Add walk compatibility
    github.com/lxn/walk

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.20
 
 require (
 	github.com/PuerkitoBio/goquery v1.8.1 // indirect
+	github.com/akavel/rsrc v0.10.2 // indirect
 	github.com/andybalholm/cascadia v1.3.2 // indirect
 	github.com/antchfx/htmlquery v1.3.0 // indirect
 	github.com/antchfx/xmlquery v1.3.17 // indirect
@@ -15,10 +16,14 @@ require (
 	github.com/golang/protobuf v1.5.3 // indirect
 	github.com/jawher/mow.cli v1.2.0 // indirect
 	github.com/kennygrant/sanitize v1.2.4 // indirect
+	github.com/lxn/walk v0.0.0-20210112085537-c389da54e794 // indirect
+	github.com/lxn/win v0.0.0-20210218163916-a377121e959e // indirect
 	github.com/saintfish/chardet v0.0.0-20230101081208-5e3ef4b5456d // indirect
 	github.com/temoto/robotstxt v1.1.2 // indirect
 	golang.org/x/net v0.11.0 // indirect
+	golang.org/x/sys v0.9.0 // indirect
 	golang.org/x/text v0.10.0 // indirect
 	google.golang.org/appengine v1.6.7 // indirect
 	google.golang.org/protobuf v1.31.0 // indirect
+	gopkg.in/Knetic/govaluate.v3 v3.0.0 // indirect
 )
--- main.go
+++ main.go
@@ -1,19 +1,20 @@
 package main
 
-import(
-	"fmt"
+import (
 	"bufio"
-	"os"
+	"fmt"
 	"io"
+	_ "math"
+	"os"
+	_ "strconv"
 	"strings"
 	"time"
-	_ "math"
-	_ "strconv"
 
-	// scraper
-	"github.com/gocolly/colly"
-	// time.Duration is stupid. This helps convert hh:mm:ss to duration obj
-	"github.com/dannav/hhmmss"
+	"github.com/dannav/hhmmss"          // convert hh:mm:ss to duration()
+	_ "github.com/gocolly/colly"        // scraper
+	"github.com/lxn/walk"               // frontend
+	. "github.com/lxn/walk/declarative" // dependency
+	_ "github.com/lxn/win"              // dependency
 )
 
 type song struct {
@@ -21,19 +22,23 @@ type song struct {
 	Time string
 }
 
-// type dur struct {
-// 	Hours int
-// 	Minutes int
-// 	Seconds int
-// }
+var (
+	window *walk.MainWindow
+	width  int = 320
+	height int = 240
+	// x      int = 200
+	// y      int = 200
+)
 
-func main() {
-	var(
-		artist string 
-		album string
-		year string
-		name string
-		time string
+// CLI MAIN
+//
+/*func main() {
+	var (
+		artist string
+		album  string
+		year   string
+		name   string
+		time   string
 	)
 
 	fmt.Println("-------- glvdc --------")
@@ -41,32 +46,56 @@ func main() {
 	fmt.Println("")
 	url := urlprompt("bc url:")
 	ctn := catprompt("cat no:")
-	
+
 	c := colly.NewCollector()
 	n := []song{} // create array of struct
 
 	// callbacks
 	c.OnHTML("body", func(e *colly.HTMLElement) {
 		e.ForEach("div.title", func(_ int, el *colly.HTMLElement) {
-			name = el.ChildText(".track-title") // track name
+			name = el.ChildText(".track-title")        // track name
 			time = el.ChildText(".time.secondaryText") // track time
 			n = append(n, song{name, time})
 		})
 
 		e.ForEach("#name-section", func(_ int, el *colly.HTMLElement) {
 			album = el.ChildText(".trackTitle") // album name
-			artist = el.ChildText("h3 span") // artist name
+			artist = el.ChildText("h3 span")    // artist name
 		})
 
 		e.ForEach(".tralbumData.tralbum-credits", func(_ int, el *colly.HTMLElement) {
-			date,_,_ := strings.Cut(strings.TrimSpace(el.Text), "\n") // full date (released Jan 01, 20xx)
-			year = date[len(date)-4:] // grab year
+			date, _, _ := strings.Cut(strings.TrimSpace(el.Text), "\n") // full date (released Jan 01, 20xx)
+			year = date[len(date)-4:]                                   // grab year
 		})
 	})
 
 	c.Visit(url) // visit, get results
 
 	write(artist, album, year, ctn, n)
+}*/
+
+func main() {
+	MainWindow{
+		Title:  "glvdc",
+		Layout: VBox{},
+		//AssignTo: &window,
+		Size: Size{320, 240},
+		Children: []Widget{
+			Label{Text: "----glvdc----"},
+			PushButton{
+				Text: "Upload",
+				OnClicked: func() {
+					walk.App().Exit(0)
+				},
+			},
+			PushButton{
+				Text: "Quit",
+				OnClicked: func() {
+					walk.App().Exit(0)
+				},
+			},
+		},
+	}.Run()
 }
 
 // this function writes to the output.txt file and echoes
@@ -82,35 +111,35 @@ func write(ar string, al string, yr string, cat string, n []song) {
 	fmt.Println("\n-----------------------\n")
 
 	// write header
-	_, err = fmt.Fprintf(w, 
-	"Label: Geometric Lullaby\n" +
-	"Artist: %s\n" +
-	"Album: %s\n" +
-	"Year: %s\n" +
-	"(GEO - %s)\n",
-	ar, al, yr, cat)
+	_, err = fmt.Fprintf(w,
+		"Label: Geometric Lullaby\n"+
+			"Artist: %s\n"+
+			"Album: %s\n"+
+			"Year: %s\n"+
+			"(GEO - %s)\n",
+		ar, al, yr, cat)
 
 	_, err = fmt.Fprint(w,
-	"\nDownload for free or purchase at: https://geometriclullaby.bandcamp.com/\n",
+		"\nDownload for free or purchase at: https://geometriclullaby.bandcamp.com/\n",
 	)
 
 	// save first track info and remove from slice
-	var(
+	var (
 		fname string = n[0].Name
 		ftime string = n[0].Time
 	)
 	n = n[1:] // remove first track info
 
 	_, err = fmt.Fprintf(w,
-	"\n00:00:00 - %s\n",
-	fname)
+		"\n00:00:00 - %s\n",
+		fname)
 
 	ftparsed, _ := hhmmss.Parse(fc(ftime))
 	added_dur := time.Duration(int64(ftparsed/time.Second)) * time.Second
 
 	for _, element := range n {
 		_, err = fmt.Fprintf(w,
-			fmtDuration(added_dur) + " - %s\n",
+			fmtDuration(added_dur)+" - %s\n",
 			element.Name,
 		)
 		if err != nil {
@@ -157,7 +186,7 @@ func urlprompt(label string) string {
 		fmt.Fprint(os.Stderr, label+" ")
 		s, _ = r.ReadString('\n')
 		if strings.HasPrefix(s, "https://") {
-			break;
+			break
 		}
 	}
 	return strings.TrimSpace(s)
@@ -170,7 +199,7 @@ func catprompt(label string) string {
 		fmt.Fprint(os.Stderr, label+" ")
 		s, _ = r.ReadString('\n')
 		if s != "" {
-			break;
+			break
 		}
 	}
 	return strings.TrimSpace(s)
